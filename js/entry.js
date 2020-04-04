a6582193f251fa7c05e05a300d2a66f578a92320
nihilietzche <63120747+nihilietzche@users.noreply.github.com>
2020-04-04 06:09:41 +0300

    Add files via upload

---
+++ js/entry.js
@@ -0,0 +1,13 @@
+var request = require("request");
+
+request({
+  uri: "https://kursovayaforms.wufoo.com/api/v3/forms/z1ko8ngn1wlql1y/entries/count.json?sort=EntryId&sortDirection=DESC",
+  method: "GET",
+  auth: {
+    'username': 'XIT0-VHF1-V0CX-K3H0',
+    'password': 'fqw',
+    'sendImmediately': false
+  }
+}, function(error, response, body) {
+  console.log(body);
+});
